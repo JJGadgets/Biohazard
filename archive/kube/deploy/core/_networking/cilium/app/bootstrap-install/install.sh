c3f8d375e10c811c39d521a153182c162dbdf345
JJGadgets <git@jjgadgets.tech>
2023-11-29 12:33:58 +0800

    chore: cleanup

---
+++ archive/kube/deploy/core/_networking/cilium/app/bootstrap-install/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+## one of these days, I'll learn and switch to Taskfiles
+set -euo pipefail
+GITROOT=$(git rev-parse --show-toplevel)
+source <(sops -d $1 | yq .data | sed -re 's/^/export /g' | sed -e 's/: /="/g' | sed -re 's/$/"/g')
+kustomize build $2 --enable-helm | envsubst
